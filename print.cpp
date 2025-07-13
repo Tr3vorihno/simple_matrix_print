63751465413756867bafb0219ecc463fe0a1da65
Enrico Cervi <167795056+Tr3vorihno@users.noreply.github.com>
2025-07-13 21:24:06 +0200

    Create print.cpp

---
+++ print.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int lenght(char v[]);
+void riempiStr(char v[],int dim);
+char str[]="Lorem ipsum dolor sit amet";
+
+
+int main(){
+	const int dim = lenght(str);
+	char b[dim];
+	//riempiStr(b,dim); // used to put a "filler" into the printed text
+	int i=0;
+	for(int j=0; j<dim; j++){
+		char a = '!';
+		while(a!=str[i]){
+			cout<<b<<a<<endl;
+			a++;
+			//system("cls");
+		}
+		b[j] = a;
+		i++;
+	}
+	cout<<b;
+   return 0;
+}
+int lenght(char v[]){
+	int i=0;
+	for(i=0; v[i]!='\0'; i++);
+	return i;
+}
+void riempiStr(char v[],int dim){
+	int i;
+	for(i=0; i<dim; i++){
+		v[i]='*';
+	}
+	v[i]='\0';
+}
