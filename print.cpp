0d3e2e3ae2386ab7ce279c581e80308d877e3b5b
Enrico Cervi <167795056+Tr3vorihno@users.noreply.github.com>
2025-07-13 22:18:53 +0200

    Update print.cpp
    fixed some things

--- print.cpp
+++ print.cpp
@@ -1,45 +1,29 @@
 // 13/07/2025
 
 #include<iostream>
-
+#include<string.h>
+#include <chrono>
+#include <thread>
 using namespace std;
-
-int lenght(char v[]);
-void riempiStr(char v[],int dim);
-
-
 //======================================
 //INSERT IN THIS CHAR VECTOR THE STRING YOU WANT TO PRINT
-char str[]="Lorem ipsum dolor sit amet";
+string str="Lorem ipsum dolor sit amet";
 //======================================
 
 int main(){
-	const int dim = lenght(str);
+	const int dim = str.length();
 	char b[dim];
-	//riempiStr(b,dim); // used to put a "filler" into the printed text
 	int i=0;
 	for(int j=0; j<dim; j++){
 		char a = '!';
 		while(a!=str[i]){
 			cout<<b<<a<<endl;
 			a++;
-			//system("cls");
+			this_thread::sleep_for(chrono::milliseconds(1));
 		}
 		b[j] = a;
 		i++;
 	}
 	cout<<b;
    return 0;
-}
-int lenght(char v[]){
-	int i=0;
-	for(i=0; v[i]!='\0'; i++);
-	return i;
-}
-void riempiStr(char v[],int dim){
-	int i;
-	for(i=0; i<dim; i++){
-		v[i]='*';
-	}
-	v[i]='\0';
 }
